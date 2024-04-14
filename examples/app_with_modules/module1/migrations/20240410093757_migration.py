be817f98a409fc42cfc770778363bc7b4c5a9e46
David Parlevliet <1628264+dparlevliet@users.noreply.github.com>
2024-04-15 07:47:29 +1000

    updated examples so they don't conflict with one another

--- examples/app_with_modules/module1/migrations/20240410093757_migration.py
+++ examples/app_with_modules/module1/migrations/20240410093757_migration.py
@@ -11,7 +11,7 @@ def up():
 
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://dynamodb:8000')
     dynamodb.create_table(
-        TableName='users',
+        TableName='app-with-modules-users',
         KeySchema=[ {'AttributeName': 'username', 'KeyType': 'HASH'} ],
         AttributeDefinitions=[ {'AttributeName': 'username', 'AttributeType': 'S'} ],
         ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
@@ -24,4 +24,4 @@ def down():
     """
 
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://dynamodb:8000')
-    dynamodb.Table('users').delete()
+    dynamodb.Table('app-with-modules-users').delete()
